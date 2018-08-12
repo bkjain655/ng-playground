fcf1b6f67941cd5ca122b7e79fdc52f3780a05fc
gs-bkumar <bkumar@gainsight.com>
2018-08-13 02:45:35 +0530

    first cut of receipe & shopping

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    templateUrl : "./header.component.html",
+    selector : "app-header",
+    styles : []
+})
+export class HeaderComponent{
+    constructor(){}
+}
